d6faa523145699dafcf977055a73296d22e5abf8
Prince Dhingra <princedhingra25@gmail.com>
2022-04-08 23:55:34 -0400

    Create stackOperations.cpp

---
+++ stackOperations.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+
+using namespace std;
+
+class Stack {
+    public: 
+        int *arr;
+        int top;
+        int size;
+
+    Stack(int size) {
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int data) {
+        if(top < size - 1) {
+            top++;
+            arr[top] = data;
+        } else {
+            cout<<"Stack Overflow"<<endl;
+        }
+    }
+
+    void pop() {
+        if(top >= 0) {
+            top--;
+        } else {
+            cout<<"Stack Underflow"<<endl;
+        }
+    }
+
+    int peak() {
+        if(top >= 0 && top < size)
+            return arr[top];
+        else {
+            cout<<"Stack is empty";
+            return 0;
+        }
+    }
+
+    bool isEmpty() {
+        if(top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+int main() {
+    Stack st(5);
+
+    st.push(22);
+    st.push(20);
+    st.push(18);
+    st.pop();
+    st.pop();
+    // st.pop();
+    // st.pop();
+    // st.push(22);
+    // st.push(20);
+    // st.push(101);
+
+    cout<<st.isEmpty();
+
+    return 0;
+}
